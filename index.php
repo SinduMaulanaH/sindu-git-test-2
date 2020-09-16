01ac706a816da66b5d19558503bbcd9fa7284b58
Sindu Maulana <sindu.maulana@gmail.com>
2020-09-16 14:41:54 +0700

    menambahakan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello Dude</h1>
     <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Corporis, consequatur. Mollitia dolore distinctio numquam quia quibusdam tempore enim nulla qui incidunt harum, nobis nostrum veritatis, soluta vitae laudantium debitis expedita.</p>
+    <ul>
+        <li>1</li>
+        <li>2</li>
+    </ul>
 </body>
 </html>
